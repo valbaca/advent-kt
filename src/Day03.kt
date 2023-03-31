ef1652e9f1bfd8db3f14e2aff6e15f36c10e59f5
Valentin Baca <valbaca@gmail.com>
2023-03-30 21:46:27 -0700

    Day 03 done

---
+++ src/Day03.kt
@@ -0,0 +1,23 @@
+import java.lang.invoke.MethodHandles
+
+private val day = MethodHandles.lookup().lookupClass().name.removeSuffix("Kt")
+
+fun main() {
+    day.println()
+    fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    checkEq(0, part1(readInput("${day}_ex")))
+
+    val input = readInput(day)
+
+    println("Part 1:")
+    solve { part1(input) }
+    println("Part 2:")
+    solve { part2(input) }
+}
