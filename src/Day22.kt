dba10178df03118389724e9d64ec6f553094652b
Valentin Baca <valbaca@gmail.com>
2023-04-22 13:16:29 -0700

    Day 22 done (with help)

--- src/Day22.kt
+++ src/Day22.kt
@@ -18,17 +18,19 @@ Part 2 looks incredibly tedious. Will come back to that one.
 
 private typealias Row = Int
 private typealias Col = Int
-private enum class LR {L, R;}
-private enum class NSEW{N,S,E,W;
 
-    fun rotateLeft(): NSEW = when(this) {
+private enum class LR { L, R; }
+private enum class NSEW {
+    N, S, E, W;
+
+    fun rotateLeft(): NSEW = when (this) {
         N -> W
         S -> E
         E -> N
         W -> S
     }
 
-    fun rotateRight(): NSEW = when(this) {
+    fun rotateRight(): NSEW = when (this) {
         N -> E
         S -> W
         E -> S
@@ -39,50 +41,61 @@ private enum class NSEW{N,S,E,W;
 private typealias Inst = Either<LR, Int> // either "L"|"R" or Instructions
 private typealias Grid = Table<Row, Col, Char>
 
+private var LEN = 0
+
 private data class Pos(var r: Row, var c: Col, var face: NSEW) {
-    fun follow(inst: Either<LR, Int>, table: Table<Row, Col, Char>) {
+    fun follow(inst: Either<LR, Int>, table: Table<Row, Col, Char>, part2: Boolean = false) {
         when (inst) {
             is Either.Left -> {
-                when(inst.leftOrNull()!!) {
+                when (inst.leftOrNull()!!) {
                     LR.L -> face = face.rotateLeft()
                     LR.R -> face = face.rotateRight()
                 }
             }
+
             is Either.Right -> {
                 repeat(inst.getOrNull()!!) {
-                    moveToNext(table)
+                    if (part2) {
+                        moveToNextWrapping(table)
+                    } else {
+                        moveToNext(table)
+
+                    }
                 }
             }
         }
     }
 
     private fun moveToNext(table: Table<Row, Col, Char>) {
-        var (nxtR, nxtC) = when(face) {
-            N -> r -1 to c
-            S -> r+1 to c
-            E -> r to c+1
-            W -> r to c-1
+        var (nxtR, nxtC) = when (face) {
+            N -> r - 1 to c
+            S -> r + 1 to c
+            E -> r to c + 1
+            W -> r to c - 1
         }
         if (table[nxtR, nxtC] == null) {
-            when(face) {
+            when (face) {
                 N -> {
-                    nxtR = table.rowKeySet().sortedDescending().first {
-                        r -> table[r, c] != null
+                    nxtR = table.rowKeySet().sortedDescending().first { r ->
+                        table[r, c] != null
                     }
                 }
+
                 S -> {
-                    nxtR = table.rowKeySet().sorted().first {
-                            r -> table[r, c] != null
+                    nxtR = table.rowKeySet().sorted().first { r ->
+                        table[r, c] != null
                     }
                 }
+
                 E -> {
-                    nxtC = table.columnKeySet().sorted().first {
-                        c -> table[r, c] != null
+                    nxtC = table.columnKeySet().sorted().first { c ->
+                        table[r, c] != null
                     }
                 }
+
                 W -> {
-                    nxtC = table.columnKeySet().sortedDescending().first {
-                            c -> table[r, c] != null
+                    nxtC = table.columnKeySet().sortedDescending().first { c ->
+                        table[r, c] != null
                     }
                 }
             }
@@ -93,20 +106,61 @@ private data class Pos(var r: Row, var c: Col, var face: NSEW) {
         }
     }
 
-    fun score(): Int =
-        (r+1) * 1000 + 4 * (c+1) + when(face) {
-            N -> 3
-            S -> 1
-            E -> 0
-            W -> 2
+    private fun moveToNextWrapping(table: Table<Row, Col, Char>) {
+        var (nxtR, nxtC) = when (face) {
+            N -> r - 1 to c
+            S -> r + 1 to c
+            E -> r to c + 1
+            W -> r to c - 1
         }
+        var nxtFace = face
+        if (table[nxtR, nxtC] == null) {
+            // adapted from ClouddJR again:
+            // https://github.com/ClouddJR/advent-of-code-2022/blob/main/src/main/kotlin/com/clouddjr/advent2022/Day22.kt#L47
+            val x = c
+            val y = r
+            val trip = when (face to x/50 to y/50) {
+                N to 1 to 0 -> 0 to (100 + x) to E
+                W to 1 to 0 -> 0 to (149 - y) to E
+                N to 2 to 0 -> (x - 100) to 199 to N
+                E to 2 to 0 -> 99 to (149 - y) to W
+                S to 2 to 0 -> 99 to (-50 + x) to W
+                E to 1 to 1 -> (50 + y) to 49 to N
+                W to 1 to 1 -> (y - 50) to 100 to S
+                N to 0 to 2 -> 50 to (x + 50) to E
+                W to 0 to 2 -> 50 to (149 - y) to E
+                E to 1 to 2 -> 149 to (149 - y) to W
+                S to 1 to 2 -> 49 to (100 + x) to W
+                E to 0 to 3 -> (y - 100) to 149 to N
+                S to 0 to 3 -> (x + 100) to 0 to S
+                W to 0 to 3 -> (y - 100) to 0 to S
+                else -> error("invalid state")
+            }
+            nxtC = trip.first.first // c = x
+            nxtR = trip.first.second // r = y
+            nxtFace = trip.second
+        }
+        checkNotNull(table[nxtR, nxtC])
+
+        if (table[nxtR, nxtC] == '.') {
+            r = nxtR
+            c = nxtC
+            face = nxtFace
+        }
+    }
+
+    fun score(): Int = (r + 1) * 1000 + 4 * (c + 1) + when (face) {
+        N -> 3
+        S -> 1
+        E -> 0
+        W -> 2
+    }
 
     companion object {
         fun firstPos(grid: Grid): Pos {
             for (r in grid.rowKeySet().sorted()) {
                 for (c in grid.columnKeySet().sorted()) {
-                    if (grid[r,c] == '.')
-                        return Pos(r, c, E)
+                    if (grid[r, c] == '.') return Pos(r, c, E)
                 }
             }
             throw IllegalArgumentException()
@@ -146,7 +200,6 @@ fun main() {
     }
 
 
-
     fun part1(input: List<String>): Int {
         val table = parseTable(input.subList(0, input.size - 2))
         val instructions = parseInstructions(input.last())
@@ -158,7 +211,13 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val table = parseTable(input.subList(0, input.size - 2))
+        val instructions = parseInstructions(input.last())
+        var pos = firstPos(table)
+        for (inst in instructions) {
+            pos.follow(inst, table, part2 = true)
+        }
+        return pos.score()
     }
 
     checkEq(6032, part1(readInput("${day}_ex")))
